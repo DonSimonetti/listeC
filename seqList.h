f1dd5342a63358a8d83908f2be41ad5806a9af5d
DonSimonetti <matt.simonetti@hotmail.it>
2017-12-28 21:39:09 +0100

    Added my header scutils.h and pList_search

--- seqList.h
+++ seqList.h
@@ -5,6 +5,10 @@
 #ifndef LISTEC_SEQLIST_H
 #define LISTEC_SEQLIST_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "scutils.h"
+
 struct seqList
 {
     float * buffer;
@@ -15,7 +19,20 @@ struct seqList
 
 void seqList_init(struct seqList * list,int size)
 {
+    list->buffer=(float *)malloc(sizeof(float)*size);
+    if(list->buffer==NULL)
+    {
+        printf("ERRORE: impossibile allocare memoria per lista sequenziale");
+        return;
+    }
+    list->size=size;
+    list->head=0;
+    list->tail=0;
+}
 
+void seqList_preInsert(struct seqList * list, float val)
+{
+    
 }
 
 #endif //LISTEC_SEQLIST_H
