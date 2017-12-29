36067797ffa45e76c5885fdd4a433e50467f0a53
DonSimonetti <hesoyam123>
2017-12-29 14:13:33 +0100

    Altre funzioni lista sequenziale

--- seqList.h
+++ seqList.h
@@ -30,9 +30,59 @@ void seqList_init(struct seqList * list,int size)
     list->tail=0;
 }
 
-void seqList_preInsert(struct seqList * list, float val)
+bool seqList_preInsert(struct seqList * list, float val)
 {
-    
+    if(list->head==(list->tail+1)%list->size)//lista piena
+        return false;
+    list->head=(list->head-1+list->size)%list->size;
+    list->buffer[list->head]=val;
+    return true;
+}
+
+void seqList_generate(struct seqList * list, int size, int values)
+{
+    if(values > size)
+    {
+        printf("ERRORE: si vuole generare una lista sequenziale con troppi valori");
+        return;
+    }
+    seqList_init(list,size);
+    int i;
+    for(i=0;i<values;i++)
+    {
+        float val=(float)(rand()%100);
+        seqList_preInsert(list, val);
+    }
+}
+
+bool seqList_sufInsert(struct seqList * list, float val)
+{
+    if(list->head==(list->tail+1)%list->size)//lista piena
+        return false;
+    list->buffer[list->tail]=val;
+    list->tail=(list->tail+1+list->size)%list->size;
+    return true;
+}
+
+void seqList_visit(struct seqList * list)
+{
+    int i;
+    for(i=list->head;i!=list->tail;i=(i+1)%list->size)
+    {
+        printf("\n%.3f",list->buffer[i]);
+    }
+}
+
+bool seqList_search(struct seqList * list, float val)
+{
+    bool found=false;
+    int i;
+    for(i=list->head;i!=list->tail && !found;i=(i+1)%list->size)
+    {
+        if(isEqual(val, list->buffer[i]))
+            found=true;
+    }
+    return found;
 }
 
 #endif //LISTEC_SEQLIST_H
